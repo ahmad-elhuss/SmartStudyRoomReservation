565391d31fc7a33d0ac2cbef140234475a3de71f
AHMED <elhussiny.social@gmail.com>
2026-05-08 15:38:22 +0300

    Initial project structure

---
+++ src/main/java/smartstudyroomreservation/MainApplication.java
@@ -0,0 +1,10 @@
+package smartstudyroomreservation;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Smart Study Room Reservation System Started!");
+
+    }
+}
